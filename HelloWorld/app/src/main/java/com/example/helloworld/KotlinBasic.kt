5baac8f40937f94fcb9dccda178e4a722355b6d0
simopek <>
2020-08-30 21:48:28 +0200

    Show some basic datatypes

--- HelloWorld/app/src/main/java/com/example/helloworld/KotlinBasic.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/KotlinBasic.kt
@@ -12,5 +12,25 @@ fun main() {
     immutableStr = immutableStr.toUpperCase()
     */
 
-    print(str + "!")
+    println(str + "!")
+
+    // all Integer types
+    var myByte: Byte = 12
+    var myInt: Int = 2141
+    var myShort: Short = 21
+    var myLong: Long = 123_141_421_21_1
+    /*
+    we can leave out the type if we initialize the variable because
+    there's the type inference
+    */
+
+    // without the F at the end, the type would be Double
+    var myFloat: Float = 2141.2F
+
+    var letterChar = 'A'
+    var digitChar = '1'
+
+    val yourName = "Hudson"
+    val yourNameFirstChar = yourName[0]
+    println("Your name is " + yourName + "; first char is " + yourNameFirstChar)
 }
