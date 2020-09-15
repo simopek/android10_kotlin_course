383ad54f46c1ab528229628a9e57a316d1543ae8
simopek <>
2020-09-15 22:00:11 +0200

    Add different handling on digit and decimal point buttons

--- Calculator/app/src/main/java/me/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/me/calculator/MainActivity.kt
@@ -2,10 +2,38 @@ package me.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var digitPressed = false
+    var decimalPointPressed = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onCLRButton(view: View) {
+        resultTextView.text = ""
+        digitPressed = false
+        decimalPointPressed = false
+    }
+
+    fun onDigitButton(view: View) {
+        resultTextView.append((view as Button).text)
+        digitPressed = true
+    }
+
+    fun onDecimalPointButton(view: View) {
+
+        if (!digitPressed || decimalPointPressed) {
+            return
+        }
+
+        resultTextView.append(".")
+        decimalPointPressed = true
     }
 }
