a3df46c9d63cb71134c47e85180d2239ce1bf9cf
simopek <>
2020-09-26 16:35:45 +0200

    Add refresh view on QuizQuestionsActivity

--- QuizApp/app/src/main/java/me/quizapp/QuizQuestionsActivity.kt
+++ QuizApp/app/src/main/java/me/quizapp/QuizQuestionsActivity.kt
@@ -3,12 +3,47 @@ package me.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
 class QuizQuestionsActivity : AppCompatActivity() {
+
+    private var currentQuestionIdx = 0
+    private var questions = Constants.getQuestions()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
-        Log.i("onCreate", "number of questions is ${Constants.getQuestions().size}")
+        initSubmitButton()
+        refreshView()
+    }
+
+    private fun refreshView() {
+
+        var currQuestion = questions[currentQuestionIdx]
+
+        this.questionTextView.text = currQuestion.question
+        this.questionImage.setImageResource(currQuestion.image)
+        this.questionOption1TextView.text = currQuestion.optionOne
+        this.questionOption2TextView.text = currQuestion.optionTwo
+        this.questionOption3TextView.text = currQuestion.optionThree
+        this.questionOption4TextView.text = currQuestion.optionFour
+        this.progressBar.progress = currentQuestionIdx + 1
+        this.progressTextView.text = "${(currentQuestionIdx + 1)}/${questions.size}"
+    }
+
+    private fun initSubmitButton() {
+
+        this.submitButton.setOnClickListener() {
+
+            // TODO add logic
+            currentQuestionIdx++
+
+            if (currentQuestionIdx >= questions.size) {
+                currentQuestionIdx = 0
+            }
+
+            refreshView()
+        }
     }
 }
