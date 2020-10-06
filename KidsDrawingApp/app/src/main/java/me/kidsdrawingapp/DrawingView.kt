2ee4ae1ed9468f0aef450c5d2941e5bef0f83166
simopek <>
2020-10-06 22:38:12 +0200

    Add some stuff to make the view be drawable

--- KidsDrawingApp/app/src/main/java/me/kidsdrawingapp/DrawingView.kt
+++ KidsDrawingApp/app/src/main/java/me/kidsdrawingapp/DrawingView.kt
@@ -3,6 +3,7 @@ package me.kidsdrawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
@@ -29,6 +30,74 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
         mBrushSize = 20f
     }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        canvas?.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+        if (!mDrawPath!!.isEmpty) {
+            // we can use brushThickness because mDrawPath is CustomPath
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+            canvas!!.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+        if (event == null) {
+            // if we null-check, kotlin use the smart cast
+            return false
+        }
+
+        val touchX = event.x
+        val touchY = event.y
+
+        when (event.action) {
+            // we press down
+            MotionEvent.ACTION_DOWN -> {
+
+                // "let" can be used to run
+                // a lambda expression on the target object.
+                // The elvis operator executes the method only
+                // if the reference is non-null, so by using the elvis
+                // operator and "let" we can avoid the null-check
+                mDrawPath?.let {
+
+                    it.color = color
+                    it.brushThickness = mBrushSize
+
+                    it.reset()
+                    it.moveTo(touchX, touchY)
+                }
+
+            }
+            // we keep pressing while moving
+            MotionEvent.ACTION_MOVE -> {
+
+                mDrawPath!!.lineTo(touchX, touchY)
+            }
+            // we stop touching
+            MotionEvent.ACTION_UP -> {
+                mDrawPath = CustomPath(color, mBrushSize)
+            }
+            // this way we say the event has not been handled
+            else -> return false
+        }
+
+        // so we force a redraw of the view; the onDraw method will be called
+        invalidate()
+
+        return true
+    }
+
 
     internal class CustomPath(
         var color: Int,
