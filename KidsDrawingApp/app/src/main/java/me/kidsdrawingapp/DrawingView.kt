37a9074ca71f0a563b5cf921963d25a23533e921
simopek <>
2020-10-08 22:58:44 +0200

    Add border to canvas and add dimension to brush size

--- KidsDrawingApp/app/src/main/java/me/kidsdrawingapp/DrawingView.kt
+++ KidsDrawingApp/app/src/main/java/me/kidsdrawingapp/DrawingView.kt
@@ -3,6 +3,7 @@ package me.kidsdrawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -29,7 +30,6 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20f
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -109,6 +109,14 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         invalidate()
 
         return true
+    }
+
+    fun setBrushSize(newSize: Float) {
+
+        // with TypedValue we take into account the dimension unit and we convert the size
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, newSize,
+            resources.displayMetrics)
+        mDrawPaint!!.strokeWidth = mBrushSize
     }
 
 
