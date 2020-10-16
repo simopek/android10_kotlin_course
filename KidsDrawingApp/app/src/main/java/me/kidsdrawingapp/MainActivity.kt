e311429749ecce8936a45b24c6be0cafd36b2cd2
simopek <>
2020-10-16 22:59:04 +0200

    Add buttons to select brush color

--- KidsDrawingApp/app/src/main/java/me/kidsdrawingapp/MainActivity.kt
+++ KidsDrawingApp/app/src/main/java/me/kidsdrawingapp/MainActivity.kt
@@ -1,15 +1,25 @@
 package me.kidsdrawingapp
 
 import android.app.Dialog
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var selectedColorBrushImageButton: ImageButton? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        selectedColorBrushImageButton = this.brushColorSelectorLayout[0] as ImageButton
+        selectedColorBrushImageButton!!.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.selected_palette))
 
         brushSelectorDialogButton.setOnClickListener {
             showBrushSelectorDialog()
